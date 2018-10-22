0b24087845d7b4bf893c5fccb611ffabcad05018
Graeme Congreve <junkmailgmc@gmail.com>
2018-10-22 20:01:54 +0100

    All questions answered, in A B and C, some refactoring- added alternative versions in C

--- part_c/library.rb
+++ part_c/library.rb
@@ -8,16 +8,31 @@ class Library
     return @array_of_books
   end
 
+
+  #Create a method that takes in a book title and returns all of the information about that book
+
   def info_by_title(title)
     for book in @array_of_books
       return book if :title == title
     end
   end
 
+  # def rental_info_by_title(title)
+  #   book = info_by_title(title)
+  #   return book[0][:rental_details]
+  # end
+
   def rental_info_by_title(title)
-    book = info_by_title(title)
-    return book[0][:rental_details]
-  end
+      book = info_by_title(title)
+      rental_info = "Student name: #{book[0][:rental_details][:student_name]}, Date due: #{book[0][:rental_details][:date]}"
+      return rental_info
+    end
+
+
+    def create_new(title)
+      @array_of_books.push({title: title, rental_details: {student_name: "", date: ""}})
+    end
+
 
  #  def create_new(title)
  #    for book in @array_of_books
@@ -27,9 +42,9 @@ class Library
 
 
 
- def create_new(title)
-   @array_of_books << {title: title, rental_details: {student_name: "", date: ""}}
- end
+ # def create_new(title)
+ #   @array_of_books << {title: title, rental_details: {student_name: "", date: ""}}
+ # end
 
  def change_rental_details(title, new_name, new_date)
    for book in @array_of_books
--- part_c/specs/library_specs.rb
+++ part_c/specs/library_specs.rb
@@ -16,11 +16,20 @@ def test_book_info_by_title
   p result
 end
 
+# def test_rental_info_by_title
+#   book = Library.new([{title: "lord of the rings", rental_details: {student_name: "Jeff", date: "01/12/16"}}])
+#   result = book.rental_info_by_title("lord of the rings").to_s
+#   assert_equal("{:student_name=>\"Jeff\", :date=>\"01/12/16\"}", result)
+# end
+
 def test_rental_info_by_title
   book = Library.new([{title: "lord of the rings", rental_details: {student_name: "Jeff", date: "01/12/16"}}])
   result = book.rental_info_by_title("lord of the rings").to_s
-  assert_equal("{:student_name=>\"Jeff\", :date=>\"01/12/16\"}", result)
+  assert_equal("Student name: Jeff, Date due: 01/12/16", result)
 end
+
+
+
 
 def test_add_book
   book = Library.new([{title: "", rental_details: {student_name: "", date: ""}}])
@@ -28,11 +37,17 @@ def test_add_book
   assert_equal([{title: "1984", rental_details: {student_name: "", date: ""}}], book.book_array)
 end
 
+def test_add_book
+  book = Library.new([{title: "lord of the rings", rental_details: {student_name: "Jeff", date: "01/12/16"}}])
+  book.create_new("1984")
+  assert_equal([{title: "lord of the rings", rental_details: {student_name: "Jeff", date: "01/12/16"}}, {title: "1984", rental_details: {student_name: "", date: ""}}], book.book_array)
+end
+
+
 def test_change_rental_details
   book = Library.new([{title: "lord of the rings", rental_details: {student_name: "Jeff", date: "01/12/16"}}])
   book.change_rental_details("lord of the rings", "Adam", "10/10/19")
   assert_equal([{title: "lord of the rings", rental_details: {student_name: "Adam", date: "10/10/19"}}], book.book_array)
-
 end
 
 end
